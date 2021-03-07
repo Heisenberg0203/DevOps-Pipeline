13b7784cd2b36857844a35b7294fc307fc02f600
Heisenberg0203 <rushikesh0203@gmail.com>
2021-03-08 00:57:50 +0530

    Integrated log4j

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,8 +1,14 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.Scanner;
 
+
 public class Driver {
+    static Logger logger = LogManager.getLogger(Driver.class.getName());
     public static void main(String[] args) {
         Scanner scan  = new Scanner(System.in);
+
         while(true){
             printOption();
             try{
@@ -10,56 +16,68 @@ public class Driver {
                 if(option==1){
                     System.out.println("Enter Number:");
                     try{
-                    float num = scan.nextFloat();
+                        float num = scan.nextFloat();
                         float ans = findsquareRoot(num);
                         System.out.println("SquareRoot of "+num+" is "+ans);
+                        logger.info("SquareRoot input= "+num+" output= "+ans);
                     }
                     catch (Exception ex){
                         System.out.println("Exception occured: "+ex.toString());
+                        logger.error("exception Invalid input");
+
                     }
                 }
-                if(option==2){
+                else if(option==2){
                     System.out.println("Enter Number:");
                     try{
-                    int num = scan.nextInt();
+                        int num = scan.nextInt();
                         double ans = findfactorial(num);
                         System.out.println("factoial of "+ num +" is "+ ans );
+                        logger.info("Factorial input= "+num+" output= "+ans);
                     }
                     catch (Exception ex){
                         System.out.println("Exception occured: "+ex.toString());
+                        logger.error("exception Invalid input");
                     }
                 }
-                if(option==3){
+                else if(option==3){
                     System.out.println("Enter Number:");
                     try {
                         float num = scan.nextFloat();
                         double ans = findlog(num);
                         System.out.println("Log of "+ num +" is "+ ans );
+                        logger.info("Logarithm input= "+num+" output= "+ans);
                     }
                     catch (Exception ex){
                         System.out.println("Exception occured: "+ex.toString());
+                        logger.error("exception Invalid input");
                     }
                 }
-                if(option==4){
+                else if(option==4){
                     System.out.println("Enter 2 Numbers:");
                     try {
                         float a = scan.nextInt();
                         float b = scan.nextInt();
                         double ans = findpower(a,b);
                         System.out.println(a+" raise to "+ b +" is "+ ans );
+                        logger.info("Power input= "+a+","+b+" output= "+ans);
                     }
                     catch (Exception ex){
                         System.out.println("Exception occured: "+ex.toString());
+                        logger.error("exception Invalid input");
                     }
                 }
                 else if(option==5){
+                    logger.info("exit status=0");
                     break;
                 }
                 else{
+                    logger.error("Invalid_Option invalid option chosen");
                     throw new Exception();
                 }
             }
             catch (Exception ex){
+                logger.error("exception  Invalid option");
                 System.out.println("Exception occured: "+ex.toString());
             }
             scan.nextLine();
